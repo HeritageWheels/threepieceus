6445b095e80764216d8703957ef873e428b1e1eb
Joseph Vega <javega175@gmail.com>
2026-02-08 21:28:39 -0800

    updated base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  base: "/vehicle-gallery/",
+  base: "/vehicle-gallery",
   output: "server",
   adapter: vercel(),
 });
